bf02742084ad5d32f530bbcd6447d089eaeb7366
romain-rigaud <117656441+romain-rigaud@users.noreply.github.com>
2022-12-07 14:33:05 +0100

    Add files via upload

--- prog.py
+++ prog.py
@@ -1,6 +1,48 @@
+import pathlib
+import os 
 import sys
 
-def main(argv, taille):
+#titre_doss = input('Choisir nom du dossier \n') 
+
+def recherche(fichier):
+    texte = fichier.readlines()
+
+    #titre_doss.replace(" ","_")
+    #print(titre_doss)
+
+    j = 0
+    trouver = False
+    while (j < len(texte)):
+        v=j
+        while not trouver : 
+            print(texte[v])
+            if texte[v]=='\n' :
+                trouver = True
+            v+=1
+        if not texte[j].find("Abstract") :
+
+            n=j
+            while(texte[n]!='\n') :
+                print(texte[n])
+                n+=1
+            break
+        
+        j+=1
+    print("\n\n\n")
+
+
+def main ():
+	init=0
+	for path in pathlib.Path(".").iterdir():
+		if path.is_file():
+			root, extension = os.path.splitext(path)
+			if (extension == '.txt'):
+					print (path)
+					fichier = open(path,'r')
+					recherche(fichier)
+			init+=1
+
+def test(argv,taille):
 	if ( taille < 2 ):
 		print("Argument manquant.")
 		return 1
@@ -8,35 +50,9 @@ def main(argv, taille):
 	if ( argv[1] != "-t" and argv[1] != "-x" ) :
 		print("Argument non reconnu : ",argv[1]," (deux possibilitées : -t ou -x).")
 		return 1
-		
-	fichier = open("Nomfichier",'r')
-
-	texte = fichier.readlines()
-
-	B = True
-	j = 0
-	titre = False
-
-	trouver = False
-	while (j < len(texte)):
-		v=j
-		while not trouver : 
-			print(texte[v])
-			if texte[v]=='\n' :
-				trouver = True
-			v+=1
-		if not texte[j].find("Abstract") :
-
-			n=j
-			while(texte[n]!='\n') :
-				print(texte[n])
-				n+=1
-			break
-		
-		j+=1
 	return 0
-		
 
 if __name__ == '__main__':
-	e = main(sys.argv,len(sys.argv))
-	print("Processus terminé : ",e)
+	e = test(sys.argv,len(sys.argv))
+	if ( e == 0 ):
+		main();
